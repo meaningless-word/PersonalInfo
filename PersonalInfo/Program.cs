59fba78fe46313b5926e05f81fb35d1ab12f21c4
Роман Толмачёв <unifiq@gmail.com>
2020-12-17 10:43:13 +0700

    Добавьте файлы проекта.

---
+++ PersonalInfo/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace PersonalInfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string MyName = "Jane";
+            byte MyAge = 27;
+            bool HaveIApet = true;
+            double MyShoeSize = 37.5;
+
+            Console.WriteLine("My name is " + MyName);
+            Console.WriteLine("My age is " + MyAge);
+            Console.WriteLine("Do I have a pet? " + HaveIApet);
+            Console.WriteLine("My shoe size is " + MyShoeSize);
+
+            Console.ReadKey();
+
+        }
+    }
+}
